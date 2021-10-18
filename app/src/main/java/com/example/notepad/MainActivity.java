e58daf6ad3e566cb80b00f5de0d64f3c51566429
megapxdll <mkuanyshbek97@mail.ru>
2021-10-18 10:37:38 +0600

    Added city fragment class and xml. Added cities list.

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.notepad;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Создаем фрагмент
+        CitiesFragment citiesFragment = new CitiesFragment();
+        // Вызываем FragmentManager
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, citiesFragment).commit();
     }
 }
