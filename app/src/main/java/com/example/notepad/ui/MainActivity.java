9ca16b170d3669d3e1baaea8efc51dd9adf90a34
Madi <mkuanyshbek97@mail.ru>
2021-10-28 00:04:55 +0600

    Attempt to add functionality to buttons

--- app/src/main/java/com/example/notepad/ui/MainActivity.java
+++ app/src/main/java/com/example/notepad/ui/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentResultListener;
 
@@ -18,6 +21,7 @@ import com.example.notepad.ui.details.NotePadDetailsActivity;
 import com.example.notepad.ui.details.NotePadDetailsFragment;
 import com.example.notepad.ui.fm.FmActivity;
 import com.example.notepad.ui.list.NotePadListFragment;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String ARG_NOTE = "ARG_NOTE";
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initToolbarAndDrawer();
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -105,5 +110,40 @@ public class MainActivity extends AppCompatActivity {
             outState.putParcelable(ARG_NOTE, selectedNotePad);
         }
         super.onSaveInstanceState(outState);
+    }
+
+    private void initToolbarAndDrawer() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initDrawer(toolbar);
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+
+        final DrawerLayout drawer = findViewById(R.id.navigation_menu);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.action_drawer_about:
+                        Toast.makeText(getApplicationContext(), "About window opened", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.action_drawer_exit:
+                        Toast.makeText(getApplicationContext(), "Settings window opened", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
